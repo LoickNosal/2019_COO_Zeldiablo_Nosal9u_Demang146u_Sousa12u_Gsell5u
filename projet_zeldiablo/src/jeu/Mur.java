a681301644c9d6d38408484c2aa4a2bb1e177cbf
Nosal Loïck <loick.nosal5@etu.univ-lorraine.fr>
2019-05-31 11:31:58 +0200

    Tests et classes Cases

---
+++ projet_zeldiablo/src/jeu/Mur.java
@@ -0,0 +1,24 @@
+package src.jeu;
+/**
+ * @author Nosal Loïck
+ * Classe qui modélise un mur
+ */
+public class Mur extends Case{
+	
+	/**
+	 * Constructeur de base d'un Mur
+	 * @param x coordonnées x du mur
+	 * @param y coordonnées y du mur
+	 */
+	public Mur(int x, int y) {
+		if (x < 0) {
+			x = 0;
+		}
+		if (y < 0) {
+			y = 0;
+		}
+		this.posX = x;
+		this.posY = y;
+	}
+
+}
