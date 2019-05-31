5bacecd2de768224771f6e49289bd782a9f40234
Paul Gsell <paulgsell57@gmail.com>
2019-05-31 11:25:58 +0200

    Creation de la classe Entite et Aventurier et ajout de la fonction seDeplacer pour Entite

---
+++ projet_zeldiablo/src/jeu/Aventurier.java
@@ -0,0 +1,8 @@
+public class Aventurier extends Entite {
+
+    public Aventurier(int p, int x, int y, String n)
+    {
+        super(p,x,y,n);
+    }
+
+}
--- projet_zeldiablo/src/jeu/Entite.java
+++ projet_zeldiablo/src/jeu/Entite.java
@@ -1,3 +1,42 @@
 abstract public class Entite{
-    
+
+    private int pv;
+    private int x;
+    private int y;
+    private String nom;
+
+    public Entite(int p, int x, int y, String n)
+    {
+        this.pv = p;
+        this.x = x;
+        this.y = y;
+        this.n = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void seDeplacer(char cardinaux)
+    {
+        switch(cardinaux)
+        {
+            case 'N': this.y-- break;
+            case 'E': this.x++ break;
+            case 'S': this.y++ break;
+            case 'O': this.x-- break;
+        }
+    }
 }
