80d59a28cc8f5f35c57295b859d3598c0142f711
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 10:28:13 +0200

    Debbugage de Entite et CaseVide, Finition du premier TestEntite

--- projet_zeldiablo/src/tests/TestEntite.java
+++ projet_zeldiablo/src/tests/TestEntite.java
@@ -1,9 +1,9 @@
 package tests;
 
 import jeu.Aventurier;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.*;
 
 /**
  * Classe de test pour les Entites
@@ -14,6 +14,16 @@ public class TestEntite {
     @Test
     public void testConstructeurAventurier() {
         Aventurier a = new Aventurier(10, 0,0, "LOL");
+        assertEquals("Le constructeur doit fonctionner normalement (PV)", 10, a.getPv());
+        assertEquals("Le constructeur doit fonctionner normalement (POS X)", 0, a.getX());
+        assertEquals("Le constructeur doit fonctionner normalement (POS Y)", 0, a.getY());
+        assertEquals("Le constructeur doit fonctionner normalement (NOM)", true, a.getNom().equals("LOL"));
+
+        Aventurier p = new Aventurier(-1, 5,-5, "");
+        assertEquals("Le constructeur doit fonctionner normalement (PV)", 1, p.getPv());
+        assertEquals("Le constructeur doit fonctionner normalement (POS X)", 5, p.getX());
+        assertEquals("Le constructeur doit fonctionner normalement (POS Y)", -5, p.getY());
+        assertEquals("Le constructeur doit fonctionner normalement (NOM)", true, p.getNom().equals("Entite"));
     }
 
     @Test
