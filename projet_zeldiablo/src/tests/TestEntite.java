beba608c323361ac8f2c8680c7f048fafc042605
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 10:15:05 +0200

    Debbugage de Entite

--- projet_zeldiablo/src/tests/TestEntite.java
+++ projet_zeldiablo/src/tests/TestEntite.java
@@ -1,14 +1,19 @@
 package tests;
 
+import jeu.Aventurier;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+/**
+ * Classe de test pour les Entites
+ * @author Paul Gsell
+ */
 public class TestEntite {
 
     @Test
     public void testConstructeurAventurier() {
-        fail();
+        Aventurier a = new Aventurier(10, 0,0, "LOL");
     }
 
     @Test
