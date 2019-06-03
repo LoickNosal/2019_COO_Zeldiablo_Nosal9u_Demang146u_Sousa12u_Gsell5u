da2bb0466451867e62604c91269b68f96a3fcdc4
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 17:07:19 +0200

    Test labyrinthe

--- projet_zeldiablo/src/jeu/JeuPrincipal.java
+++ projet_zeldiablo/src/jeu/JeuPrincipal.java
@@ -10,7 +10,17 @@ public class JeuPrincipal {
     public static final int level = 1;
 
     public JeuPrincipal() {
-        this.lab = new Labyrinthe();
+        this.lab = new Labyrinthe(
+                "xxxxxxxxxx\n" +
+                "x-x------x\n" +
+                "x-xxxxxx-x\n" +
+                "x-x------x\n" +
+                "x-x--xx--x\n" +
+                "x-x--xx--x\n" +
+                "x-x------x\n" +
+                "x-x------x\n" +
+                "x--------x\n" +
+                "xxxxxxxxxx\n");
         this.av = new Aventurier(10, 100,100, "Aventurier", this.lab);
     }
 
