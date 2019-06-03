0ec3e63df6bf7db0557df0bd97847664d6fa11c9
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 15:07:12 +0200

    Update MainPerso.java

--- projet_zeldiablo/src/moteurJeu/MainPerso.java
+++ projet_zeldiablo/src/moteurJeu/MainPerso.java
@@ -23,7 +23,7 @@ public class MainPerso {
 		
 		// lance la boucle de jeu qui tourne jusque la fin du jeu
 		try {
-			moteur.lancerJeu(600, 600);
+			moteur.lancerJeu(800, 800);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
