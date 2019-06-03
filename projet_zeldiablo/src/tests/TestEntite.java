1d3c3f55ab7e37b0d1bdab508aae6f62d64b398c
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 10:29:59 +0200

    Changement dans le test de TestEntite n°1

--- projet_zeldiablo/src/tests/TestEntite.java
+++ projet_zeldiablo/src/tests/TestEntite.java
@@ -15,20 +15,20 @@ public class TestEntite {
     public void testConstructeurAventurier() {
         Aventurier a = new Aventurier(10, 0,0, "LOL");
         assertEquals("Le constructeur doit fonctionner normalement (PV)", 10, a.getPv());
-        assertEquals("Le constructeur doit fonctionner normalement (POS X)", 0, a.getX());
-        assertEquals("Le constructeur doit fonctionner normalement (POS Y)", 0, a.getY());
         assertEquals("Le constructeur doit fonctionner normalement (NOM)", true, a.getNom().equals("LOL"));
 
         Aventurier p = new Aventurier(-1, 5,-5, "");
-        assertEquals("Le constructeur doit fonctionner normalement (PV)", 1, p.getPv());
-        assertEquals("Le constructeur doit fonctionner normalement (POS X)", 5, p.getX());
-        assertEquals("Le constructeur doit fonctionner normalement (POS Y)", -5, p.getY());
-        assertEquals("Le constructeur doit fonctionner normalement (NOM)", true, p.getNom().equals("Entite"));
+        assertEquals("Le constructeur 2 doit fonctionner normalement (PV)", 1, p.getPv());
+        assertEquals("Le constructeur 2 doit fonctionner normalement (NOM)", true, p.getNom().equals("Entite"));
     }
 
     @Test
     public void testSeDeplacer() {
-        fail();
+        Aventurier a = new Aventurier(10, 0,0, "LOL");
+        a.seDeplacer('N');
+        a.seDeplacer('E');
+        a.seDeplacer('E');
+        a.seDeplacer('E');
     }
 
 
