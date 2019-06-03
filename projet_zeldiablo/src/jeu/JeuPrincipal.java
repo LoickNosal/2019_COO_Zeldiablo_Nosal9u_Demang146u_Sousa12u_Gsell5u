bc3760f8ba9d256d28cc8bc6798be2a400cc328e
Paul Gsell <paulgsell57@gmail.com>
2019-06-03 16:54:15 +0200

    Update JeuPrincipal.java

--- projet_zeldiablo/src/jeu/JeuPrincipal.java
+++ projet_zeldiablo/src/jeu/JeuPrincipal.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 public class JeuPrincipal {
 
     public Labyrinthe lab;
-    public ArrayList<MonstreImmobile> mi;
     public Aventurier av;
 
     public static final int level = 1;
 
     public JeuPrincipal() {
         this.lab = new Labyrinthe();
-        this.mi = new ArrayList<MonstreImmobile>();
         this.av = new Aventurier(10, 100,100, "Aventurier", this.lab);
     }
 
