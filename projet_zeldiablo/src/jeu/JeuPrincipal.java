15296ddb4aae520ffe5d1598d52312daab122019
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-06-04 10:55:46 +0200

    Amelioration de chargement du JSON

--- projet_zeldiablo/src/jeu/JeuPrincipal.java
+++ projet_zeldiablo/src/jeu/JeuPrincipal.java
@@ -5,70 +5,96 @@ import org.json.simple.parser.*;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 /**
  * modelise le jeu principal
+ * @author Louis Demange
  */
 public class JeuPrincipal {
 
     public Labyrinthe labyrinthe;
     public Aventurier aventurier;
-    public static final int level = 1;
+    public int level;
 
 
+    /**
+     * construit le modèle du jeu
+     */
     public JeuPrincipal() {
-        chargerLVL(3);
+        level = 1;
+        chargerLVL(level);
         this.aventurier = new Aventurier(10, 100, 100, "Aventurier", this.labyrinthe);
     }
 
 
+    /**
+     * charge un niveau a partir du numero du niveau
+     * @param lvl numero du niveau
+     */
     private void chargerLVL(int lvl) {
         try {
-            JSONParser parser = new JSONParser();
-            String ressourcesPath = URLDecoder.decode(getClass().getClassLoader().getResource("./").getPath(), "UTF-8");
-            Object obj;
             JSONObject json;
-
             switch (lvl) {
                 case 1:
-                    obj = parser.parse(new FileReader(ressourcesPath + "lvl1.json"));
-                    json = (JSONObject) obj;
-                    this.labyrinthe = chargerLabyrinthe(json);
+                    json = chargerJSON("lvl1.json");
+                    this.labyrinthe = decoderJSON(json);
                     break;
                 case 2:
-                    obj = parser.parse(new FileReader(ressourcesPath + "lvl2.json"));
-                    json = (JSONObject) obj;
-                    this.labyrinthe = chargerLabyrinthe(json);
+                    json = chargerJSON("lvl2.json");
+                    this.labyrinthe = decoderJSON(json);
                     break;
                 case 3:
-                	obj = parser.parse(new FileReader(ressourcesPath + "lvl3.json"));
-                    json = (JSONObject) obj;
-                    
-                    this.labyrinthe = chargerLabyrinthe(json);
-                   
+                    json = chargerJSON("lvl3.json");
+                    this.labyrinthe = decoderJSON(json);
                     break;
                 default:
                     this.labyrinthe = new Labyrinthe();
             }
-
         } catch (ParseException | IOException e) {
             this.labyrinthe = new Labyrinthe();
         }
     }
 
 
-    private Labyrinthe chargerLabyrinthe(JSONObject json) {
+    /**
+     * charge un JSON a partir
+     * @param fichier nom du fichier JSON
+     * @return le fichier JSON
+     * @throws IOException erreur dans l'url du fichier
+     * @throws ParseException erreur avec le JSON
+     */
+    private JSONObject chargerJSON(String fichier) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        String path = URLDecoder.decode(Objects.requireNonNull(getClass().getClassLoader().getResource(fichier)).getPath(), StandardCharsets.UTF_8);
+        Object obj = parser.parse(new FileReader(path));
+        return (JSONObject) obj;
+    }
+
+
+    /**
+     * construit un labyrinthe a partir d'un JSON
+     * @param json fichier JSON comtenant les informations du niveau
+     * @return le labyrinthe correspondant au fichier JSON
+     */
+    private Labyrinthe decoderJSON(JSONObject json) {
         JSONArray jsonArray = (JSONArray) json.get("labyrinthe");
         String[] modeleLabyrinthe = new String[jsonArray.size()];
-
         for (int i = 0; i < modeleLabyrinthe.length; i++) {
             modeleLabyrinthe[i] = (String) jsonArray.get(i);
-
         }
-
         return new Labyrinthe(modeleLabyrinthe);
+    }
+
+
+    /**
+     * retourne le niveau actuel
+     * @return le niveau
+     */
+    public int getLVL() {
+        return level;
     }
 
 }
