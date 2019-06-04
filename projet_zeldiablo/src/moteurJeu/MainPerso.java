b63af3340698b10132b28292d8c41362ac8ce0a9
Paul Gsell <paulgsell57@gmail.com>
2019-06-04 08:59:17 +0200

    Debbugage

--- projet_zeldiablo/src/moteurJeu/MainPerso.java
+++ projet_zeldiablo/src/moteurJeu/MainPerso.java
@@ -24,6 +24,7 @@ public class MainPerso {
 		// lance la boucle de jeu qui tourne jusque la fin du jeu
 		try {
 			moteur.lancerJeu(900, 900);
+			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
