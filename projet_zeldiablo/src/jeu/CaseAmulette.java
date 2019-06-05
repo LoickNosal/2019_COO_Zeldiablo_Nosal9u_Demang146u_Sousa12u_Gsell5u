0b1d0734d3632847a7b3849caa177b30c5eea781
Nosal Loïck <loick.nosal5@etu.univ-lorraine.fr>
2019-06-05 11:56:36 +0200

    ajout caseAmulette

---
+++ projet_zeldiablo/src/jeu/CaseAmulette.java
@@ -0,0 +1,43 @@
+package jeu;
+
+/**
+ * represente la case de fin du jeu
+ * @author Loick
+ *
+ */
+public class CaseAmulette extends Case{
+
+	
+	/*
+	 * Constructeur
+	 */
+	public CaseAmulette(int x, int y) {
+		if (x < 0) {
+		x = 0;
+		}
+		if (y < 0) {
+		y = 0;
+		}
+		this.posX = x;
+		this.posY = y;
+		this.estPasseDessus = false;
+	}
+	
+	@Override
+	public boolean peutTraverser() {
+		return true;
+	}
+
+	@Override
+	public int typeCase() {
+		return 5;
+	}
+	
+	public String toString() {
+		return "a";
+	}
+	
+	
+	
+
+}
